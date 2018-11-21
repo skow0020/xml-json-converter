a6892ae131e8065914a50670f7967edacfea3017
Colin Skow <cskow@tapqa.com>
2018-11-21 13:56:15 -0600

    Fix and add stuff

--- src/App.js
+++ src/App.js
@@ -3,31 +3,66 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
-let parser = require('fast-xml-parser');
+import parser from 'fast-xml-parser';
+let Parser = require("fast-xml-parser").j2xParser;
 
 class App extends Component {
-  onClick() {
+  generateJSON() {
     let xmlElementContent = document.getElementById('xml-file-content').textContent;
+    let isValid = parser.validate(xmlElementContent);
 
-    if (parser.validate(xmlElementContent) === false) {
-      alert("Invalid xml!")
-    }
-    else {
-      let tObj = parser.getTraversalObj(xmlElementContent);
-      let jsonObjConverted = parser.convertToJson(tObj);
-      document.getElementById('json-file-content').textContent = JSON.stringify(jsonObjConverted, null, 2)
-    }
-  }
-
-  readXmlFile(e) {
-    let file = e.target.files[0];
-    if (!file) {
+    if (xmlElementContent === "") {
+      alert("Select an xml document")
       return;
     }
+    else if (isValid !== true) {
+      alert(`Invalid XML: ${isValid.err.msg}`);
+      return;
+    }
+    
+    let element = document.getElementById('json-file-content');
+    let tObj = parser.getTraversalObj(xmlElementContent);
+    let jsonObjConverted = parser.convertToJson(tObj);
+    element.hidden = false;
+    element.textContent = JSON.stringify(jsonObjConverted, null, 2)
+  }
+
+  generateXML() {
+    let jsonElementContent = document.getElementById('json-file-content').textContent;
+    let element = document.getElementById('xml-file-content');
+
+    if (jsonElementContent === "") {
+      alert("Select a json document")
+      return;
+    }
+    let parser = new Parser({
+      format: true,
+      indentBy: "  ",
+    });
+
+    try {      
+      element.textContent = parser.parse(JSON.parse(jsonElementContent));
+      element.hidden = false;
+    } catch (error) {
+      alert(`Unable to generate json: ${error.message}`);
+    }
+    
+  }
+
+  readFile(e) {
+    let file = e.target.files[0];
+    let element;
+    if (!file) return;
+    if (file.type === "application/json") element = document.getElementById('json-file-content');
+    else if (file.type === "text/xml") element = document.getElementById('xml-file-content');
+    else {
+      alert("File must be xml or json")
+      return;
+    }
+
     let reader = new FileReader();
     reader.onload = function (e) {
       let contents = e.target.result;
-      let element = document.getElementById('xml-file-content');
       element.hidden = false
       element.textContent = contents;
     };
@@ -39,11 +74,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Input id="xml-file-input" type="file" onChange={(e) => this.readXmlFile(e)} />
-          <Button id="run-validation" variant="contained" color="primary" onClick={this.onClick}>
-            Generate JSON
+          <Input id="file-input" type="file" onChange={(e) => this.readFile(e)} />
+          <div id="container">
+            <Button id="generate-json" variant="contained" color="primary" onClick={this.generateJSON}>
+              Generate JSON
           </Button>
-
+            <Button id="generate-xml" variant="contained" color="primary" onClick={this.generateXML}>
+              Generate XML
+          </Button>
+          </div>
         </header>
         <body>
           <div class="content-container">
