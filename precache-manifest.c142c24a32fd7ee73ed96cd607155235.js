da9ce73d06c0a593d7e20b73a94ead688d948b5b
Colin <skow0020@gmail.com>
2019-03-11 20:30:27 +0000

    Updates

---
+++ precache-manifest.c142c24a32fd7ee73ed96cd607155235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xml-json-converter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f90861f34406ac1aa76",
+    "url": "/xml-json-converter/static/js/runtime~main.8f90861f.js"
+  },
+  {
+    "revision": "225485e913b6d10d79b9",
+    "url": "/xml-json-converter/static/js/main.225485e9.chunk.js"
+  },
+  {
+    "revision": "ac965ebbc1d7b2bd1ada",
+    "url": "/xml-json-converter/static/js/1.ac965ebb.chunk.js"
+  },
+  {
+    "revision": "225485e913b6d10d79b9",
+    "url": "/xml-json-converter/static/css/main.45ab7b7f.chunk.css"
+  },
+  {
+    "revision": "6bcd18e268708bf80a65c67b44459827",
+    "url": "/xml-json-converter/index.html"
+  }
+];
